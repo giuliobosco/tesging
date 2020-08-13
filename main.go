9bad40649c0d45485dec72d0e565f42045490163
giuliobosco <giuliobva@gmail.com>
2020-08-13 18:39:33 +0200

    add testing

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
+	setupServer().Run()
+}
+
+func setupServer() *gin.Engine {
 	r := gin.Default()
 
 	// register the ping endpoint
 	r.GET("/ping", pingEndpoint)
 
-	r.Run()
+	return r
 }
 
 func pingEndpoint(c *gin.Context) {
