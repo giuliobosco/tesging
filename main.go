30a60de40f28194c979477a2ce3be4652442fee6
giuliobosco <giuliobva@gmail.com>
2020-08-14 18:39:23 +0200

    use templates

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	setupServer().Run()
@@ -12,6 +16,10 @@ func setupServer() *gin.Engine {
 	// register the ping endpoint
 	r.GET("/ping", pingEndpoint)
 
+	r.LoadHTMLGlob("templates/*")
+
+	r.GET("/", indexEndpiont)
+
 	return r
 }
 
@@ -19,4 +27,14 @@ func pingEndpoint(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "pong",
 	})
+}
+
+func indexEndpiont(c *gin.Context) {
+	c.HTML(
+		http.StatusOK,
+		"index.html",
+		gin.H{
+			"title": "Home Page",
+		},
+	)
 }
